ead16617f51ad0094182baa8e6e6fa4722329d3a
Abhinav1735 <abhinavkumawat442@gmail.com>
2026-07-25 23:08:50 +0530

    feat: Sidebar completely updated

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const sidebar = document.querySelector("[data-sidebar]");
+const sidebarBtn = document.querySelector("[data-sidebar-btn]");
+
+sidebarBtn.addEventListener("click", function () {
+  sidebar.classList.toggle("active");
+});
