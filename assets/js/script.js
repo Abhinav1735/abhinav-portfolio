68bb593ea703916415fd4867d8179ec147f2d9a0
Abhinav1735 <abhinavkumawat442@gmail.com>
2026-07-26 15:00:46 +0530

    feat : navbar completed with responsive design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,24 @@
 "use strict";
 
+/*===================================
+        MORE CONTACT TOGGLE
+===================================*/
+
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
 sidebarBtn.addEventListener("click", function () {
   sidebar.classList.toggle("active");
+});
+
+/*===================================
+        NAVBAR TOGGLE
+===================================*/
+
+const navBtn = document.querySelector("[data-nav-btn]");
+const navbar = document.querySelector("[data-navbar]");
+
+navBtn.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+  navBtn.classList.toggle("active");
 });
