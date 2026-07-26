e232f3af8c6d73dc9b94ee78d31faa5cfd254b4c
Abhinav1735 <abhinavkumawat442@gmail.com>
2026-07-26 21:54:27 +0530

    Complete responsive resume section and improve mobile navigation

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,37 @@ navBtn.addEventListener("click", function () {
   navbar.classList.toggle("active");
   navBtn.classList.toggle("active");
 });
+
+//==================================
+// SELECT ELEMENTS
+//==================================
+
+const navLinks = document.querySelectorAll("[data-nav-link]");
+const pages = document.querySelectorAll("[data-page]");
+
+//==================================
+// NAVIGATION
+//==================================
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    const pageName = link.textContent.trim().toLowerCase();
+
+    pages.forEach((page) => {
+      page.classList.toggle("active", page.dataset.page === pageName);
+    });
+
+    navLinks.forEach((item) => {
+      item.classList.remove("active");
+    });
+
+    link.classList.add("active");
+
+    // Close mobile menu
+    if (window.innerWidth <= 1080) {
+      navbar.classList.remove("active");
+
+      navBtn.classList.remove("active");
+    }
+  });
+});
